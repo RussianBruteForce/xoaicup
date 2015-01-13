f21c943c575af1029d669c982d227108af93a8bd
Viktor Hackeridze <russianbruteforce@gmail.com>
2015-01-13 18:35:30 +0600

    XOAICup реализован базовый интерфейс класса

--- src/xoaicup.cpp
+++ src/xoaicup.cpp
@@ -5,3 +5,50 @@ XOAICup::XOAICup(QObject *parent) :
 {
 
 }
+
+void XOAICup::nextOne()
+{
+
+
+	if ((currentPlayer = static_cast<Player_t>(static_cast<int>(currentPlayer) + 1))
+	    == END)
+		currentPlayer = static_cast<Player_t>(1);
+}
+
+void XOAICup::checkMatrix(Point p)
+{
+	if (p.x > M_SIZE - 1 || p.y > M_SIZE - 1)
+		throw std::range_error("Point is bigger than field size!");
+
+	// Определение нижней границы
+	auto lower_boundary = [](uint8_t x)->uint8_t {
+		auto ret = x
+			   -
+			   (SERIES - 1);
+		if (ret < 0)
+			return 0;
+		else
+			return ret;
+	};
+	// Определение верхней границы
+	auto upper_boundary = [](uint8_t x)->uint8_t {
+		auto ret = x
+			   +
+			   (SERIES - 1);
+		if (ret > M_SIZE)
+			return M_SIZE;
+		else
+			return ret;
+	};
+
+	// Длина серии
+	uint8_t count_h = 0, // Горизонталь
+		count_v = 0, // Вертикаль
+		count_d = 0; // диагональ
+	/*
+	 * TODO: Тут поиск последовательности длины SERIES относительно точки
+	 * Например, для горизонтали: от x = lower_boundary(p.x) до x = upper_boundary(p.x)
+	 */
+	if (count_h >= SERIES || count_v >= SERIES || count_d >= SERIES)
+		emit Winner(currentPlayer);
+}
--- src/xoaicup.h
+++ src/xoaicup.h
@@ -2,6 +2,11 @@
 #define XOAICUP_H
 
 #include <QObject>
+#include <stdexcept>
+#include <array>
+
+#define M_SIZE 15 // размер поля
+#define SERIES 5 // серия для победы
 
 /*
  * Собственно класс платформы
@@ -9,13 +14,30 @@
 class XOAICup : public QObject
 {
 	Q_OBJECT
+
+	enum Player_t { // Игрок
+		NONE = 0,
+		TIC = 1,
+		TOE = 2,
+		END = TOE // для упрощения итерации
+	};
+	struct Point { // Точка
+		uint8_t x, y;
+	};
+
 public:
 	explicit XOAICup(QObject *parent = 0);
 
 signals:
+	void Winner(Player_t);
 
 public slots:
+	void nextOne();
 
+private:
+	std::array<std::array<Player_t, M_SIZE>, M_SIZE> field; // поле
+	Player_t currentPlayer; // Тот игрок, чей ход будет следующим
+	void checkMatrix(Point p); // Проверка матрицы на победителя из текущей позиции
 };
 
 #endif // XOAICUP_H
